fa9fa80c27794e80d63b42de96defa343b273ee3
noumanriazz <noumanriaz575@gmail.com>
2026-03-09 00:37:15 +0500

    push notification code

--- configs/firebase.js
+++ configs/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../easyvibe-notification-firebase-adminsdk-fbsvc-f2149d0bd5.json");
+const serviceAccount = require("./easyvibe-notification-firebase-adminsdk-fbsvc-f2149d0bd5.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
